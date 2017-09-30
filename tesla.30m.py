2ccb3b9565d82b1eb135e0ddbf49c0a1bf0a69dc
Jason Baker <therippa@gmail.com>
2017-09-30 15:50:20 -0700

    remove pastebin client

--- tesla.30m.py
+++ tesla.30m.py
@@ -66,7 +66,14 @@ class Connection(object):
         self.proxy_url = proxy_url
         self.proxy_user = proxy_user
         self.proxy_password = proxy_password
-        tesla_client = self.__open("/raw/0a8e0xTJ", baseurl="http://pastebin.com")
+        tesla_client = {
+            'v1': {
+                'id': 'e4a9949fcfa04068f59abb5a658f2bac0a3428e4652315490b659d5ab3f35a9e', 
+                'secret': 'c75f14bbadc8bee3a7594412c31416f8300256d7668ea7e6e7f06727bfb9d220', 
+                'baseurl': 'https://owner-api.teslamotors.com', 
+                'api': '/api/1/'
+            }
+        }
         current_client = tesla_client['v1']
         self.baseurl = current_client['baseurl']
         self.api = current_client['api']
