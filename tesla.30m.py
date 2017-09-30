147b54a973da6d70bd9901b1a13e567fbb6d745c
Jason Baker <therippa@gmail.com>
2017-09-30 12:14:22 -0700

    add script

---
+++ tesla.30m.py
@@ -0,0 +1,223 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# <bitbar.title>Tesla BitBar</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>therippa@gmail.com</bitbar.author>
+# <bitbar.author.github>therippa</bitbar.author.github>
+# <bitbar.desc>Control your Tesla from the menubar</bitbar.desc>
+# <bitbar.image>https://i.imgur.com/XbwiemY.png</bitbar.image>
+# <bitbar.dependencies>python</bitbar.dependencies>
+# <bitbar.abouturl>https://github.com/therippa/tesla-bitbar</bitbar.abouturl>
+#
+# by therippa@gmail.com
+
+try: # Python 3
+    from urllib.parse import urlencode
+    from urllib.request import Request, urlopen, build_opener
+    from urllib.request import ProxyHandler, HTTPBasicAuthHandler, HTTPHandler
+except: # Python 2
+    from urllib import urlencode
+    from urllib2 import Request, urlopen, build_opener
+    from urllib2 import ProxyHandler, HTTPBasicAuthHandler, HTTPHandler
+import json
+import sys
+import datetime
+import calendar
+import base64
+
+# enter your tesla.com credentials below
+USERNAME='email@gmail.com'
+PASSWORD=''
+TEMP_UNIT='F' # 'F' or whatever else, it'll end up 'C'
+
+VEHICLES = { 
+    # map vehicle ID numbers to real names
+    #'012345': 'Example Vehicle Name',
+    '068927': 'Model X',
+}
+
+# ----------------------------------
+# Thank you to Greg Glockner for the code below - https://github.com/gglockner/teslajson
+# ----------------------------------
+
+class Connection(object):
+    """Connection to Tesla Motors API"""
+    def __init__(self,
+            email='',
+            password='',
+            access_token='',
+            proxy_url = '',
+            proxy_user = '',
+            proxy_password = ''):
+        """Initialize connection object
+        
+        Sets the vehicles field, a list of Vehicle objects
+        associated with your account
+        Required parameters:
+        email: your login for teslamotors.com
+        password: your password for teslamotors.com
+        
+        Optional parameters:
+        access_token: API access token
+        proxy_url: URL for proxy server
+        proxy_user: username for proxy server
+        proxy_password: password for proxy server
+        """
+        self.proxy_url = proxy_url
+        self.proxy_user = proxy_user
+        self.proxy_password = proxy_password
+        tesla_client = self.__open("/raw/0a8e0xTJ", baseurl="http://pastebin.com")
+        current_client = tesla_client['v1']
+        self.baseurl = current_client['baseurl']
+        self.api = current_client['api']
+        if access_token:
+            self.__sethead(access_token)
+        else:
+            self.oauth = {
+                "grant_type" : "password",
+                "client_id" : current_client['id'],
+                "client_secret" : current_client['secret'],
+                "email" : email,
+                "password" : password }
+            self.expiration = 0 # force refresh
+        self.vehicles = [Vehicle(v, self) for v in self.get('vehicles')['response']]
+    
+    def get(self, command):
+        """Utility command to get data from API"""
+        return self.post(command, None)
+    
+    def post(self, command, data={}):
+        """Utility command to post data to API"""
+        now = calendar.timegm(datetime.datetime.now().timetuple())
+        if now > self.expiration:
+            auth = self.__open("/oauth/token", data=self.oauth)
+            self.__sethead(auth['access_token'],
+                           auth['created_at'] + auth['expires_in'] - 86400)
+        return self.__open("%s%s" % (self.api, command), headers=self.head, data=data)
+    
+    def __sethead(self, access_token, expiration=float('inf')):
+        """Set HTTP header"""
+        self.access_token = access_token
+        self.expiration = expiration
+        self.head = {"Authorization": "Bearer %s" % access_token}
+    
+    def __open(self, url, headers={}, data=None, baseurl=""):
+        """Raw urlopen command"""
+        if not baseurl:
+            baseurl = self.baseurl
+        req = Request("%s%s" % (baseurl, url), headers=headers)
+        try:
+            req.data = urlencode(data).encode('utf-8') # Python 3
+        except:
+            try:
+                req.add_data(urlencode(data)) # Python 2
+            except:
+                pass
+
+        # Proxy support
+        if self.proxy_url:
+            if self.proxy_user:
+                proxy = ProxyHandler({'https': 'https://%s:%s@%s' % (self.proxy_user,
+                                                                     self.proxy_password,
+                                                                     self.proxy_url)})
+                auth = HTTPBasicAuthHandler()
+                opener = build_opener(proxy, auth, HTTPHandler)
+            else:
+                handler = ProxyHandler({'https': self.proxy_url})
+                opener = build_opener(handler)
+        else:
+            opener = build_opener()
+        resp = opener.open(req)
+        charset = resp.info().get('charset', 'utf-8')
+        return json.loads(resp.read().decode(charset))
+        
+
+class Vehicle(dict):
+    """Vehicle class, subclassed from dictionary.
+    
+    There are 3 primary methods: wake_up, data_request and command.
+    data_request and command both require a name to specify the data
+    or command, respectively. These names can be found in the
+    Tesla JSON API."""
+    def __init__(self, data, connection):
+        """Initialize vehicle class
+        
+        Called automatically by the Connection class
+        """
+        super(Vehicle, self).__init__(data)
+        self.connection = connection
+    
+    def data_request(self, name):
+        """Get vehicle data"""
+        result = self.get('data_request/%s' % name)
+        return result['response']
+    
+    def wake_up(self):
+        """Wake the vehicle"""
+        return self.post('wake_up')
+    
+    def command(self, name, data={}):
+        """Run the command for the vehicle"""
+        return self.post('command/%s' % name, data)
+    
+    def get(self, command):
+        """Utility command to get data from API"""
+        return self.connection.get('vehicles/%i/%s' % (self['id'], command))
+    
+    def post(self, command, data={}):
+        """Utility command to post data to API"""
+        return self.connection.post('vehicles/%i/%s' % (self['id'], command), data)
+
+def get_name(id):
+    # check name mapping array for humanized vehicle names
+    if id in VEHICLES:
+        return VEHICLES[id]
+    else:
+        return id
+
+def convert_temp(temp):
+    if TEMP_UNIT == 'F':
+        return (temp * 1.8) + 32
+    else:
+        return temp
+
+def main():
+    # create connection to tesla account
+    c = Connection(USERNAME, PASSWORD)
+
+    # see if args are passed, if so, pass commands and bail
+    if len(sys.argv) > 1:
+        v = c.vehicles[int(sys.argv[1])]
+        v.command(sys.argv[2])
+        return
+
+    # print menu - below is icon.png encoded as base64
+    print ('|image=iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAYAAADEtGw7AAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAA/xpVFh0WE1MOmNvbS5hZG9iZS54bXAAAAAAADw/eHBhY2tldCBiZWdpbj0i77u/IiBpZD0iVzVNME1wQ2VoaUh6cmVTek5UY3prYzlkIj8+IDx4OnhtcG1ldGEgeG1sbnM6eD0iYWRvYmU6bnM6bWV0YS8iIHg6eG1wdGs9IkFkb2JlIFhNUCBDb3JlIDUuMy1jMDExIDY2LjE0NTY2MSwgMjAxMi8wMi8wNi0xNDo1NjoyNyAgICAgICAgIj4gPHJkZjpSREYgeG1sbnM6cmRmPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5LzAyLzIyLXJkZi1zeW50YXgtbnMjIj4gPHJkZjpEZXNjcmlwdGlvbiByZGY6YWJvdXQ9IiIgeG1sbnM6eG1wTU09Imh0dHA6Ly9ucy5hZG9iZS5jb20veGFwLzEuMC9tbS8iIHhtbG5zOnN0UmVmPSJodHRwOi8vbnMuYWRvYmUuY29tL3hhcC8xLjAvc1R5cGUvUmVzb3VyY2VSZWYjIiB4bWxuczp4bXA9Imh0dHA6Ly9ucy5hZG9iZS5jb20veGFwLzEuMC8iIHhtbG5zOmRjPSJodHRwOi8vcHVybC5vcmcvZGMvZWxlbWVudHMvMS4xLyIgeG1wTU06T3JpZ2luYWxEb2N1bWVudElEPSJ1dWlkOjI3MzY3NDg0MTg2QkRGMTE5NjZBQjM5RDc2MkZFOTlGIiB4bXBNTTpEb2N1bWVudElEPSJ4bXAuZGlkOkI2QzU0RTM0OURFMDExRTdBNEU0QTExMzAxRjlCQkE1IiB4bXBNTTpJbnN0YW5jZUlEPSJ4bXAuaWlkOkI2QzU0RTMzOURFMDExRTdBNEU0QTExMzAxRjlCQkE1IiB4bXA6Q3JlYXRvclRvb2w9IkFkb2JlIElsbHVzdHJhdG9yIENDIDIwMTUgKE1hY2ludG9zaCkiPiA8eG1wTU06RGVyaXZlZEZyb20gc3RSZWY6aW5zdGFuY2VJRD0ieG1wLmlpZDo2MWU4Yzc5OS1kOTYyLTRjYmUtYWI0Mi1jYWZiOWY5NjFjZWUiIHN0UmVmOmRvY3VtZW50SUQ9InhtcC5kaWQ6NjFlOGM3OTktZDk2Mi00Y2JlLWFiNDItY2FmYjlmOTYxY2VlIi8+IDxkYzp0aXRsZT4gPHJkZjpBbHQ+IDxyZGY6bGkgeG1sOmxhbmc9IngtZGVmYXVsdCI+dGVzbGFfVF9CVzwvcmRmOmxpPiA8L3JkZjpBbHQ+IDwvZGM6dGl0bGU+IDwvcmRmOkRlc2NyaXB0aW9uPiA8L3JkZjpSREY+IDwveDp4bXBtZXRhPiA8P3hwYWNrZXQgZW5kPSJyIj8+ux4+7QAAALlJREFUeNpi/P//PwMtABMDjcDQM5gFmyAjI2MAkLIHYgMgdsCh9wAQXwDig8B42oAhC4o8ZAwE74H4PpQ+D6XXA7EAFK9HkwOrxTAHi8ENUA3/0fB6KEYXB6ltIMZgkKv6oS4xgIqhGAYVM4CqmQ/SQ9BgbBjqbZjB54nRQ2yqeICDTXFyu4iDTbHBB3CwKTaY5KBgJLYQAmaa/9B0z0h2ziMiOKhq8AVaGfxwULiYcbQGobnBAAEGADCCwy7PWQ+qAAAAAElFTkSuQmCC')
+    print ('---')
+
+    # loop through vehicles, print menu with relevant info
+    for i, vehicle in enumerate(c.vehicles):
+        print get_name(vehicle['display_name'])
+        charge_state = vehicle.data_request('charge_state')
+        climate_state = vehicle.data_request('climate_state')
+        print ('--Battery Level: %s%%| color=black' % str(charge_state['battery_level']))
+        print ('--Charging State: %s| color=black' % charge_state['charging_state'])
+        print ('-----')
+        try:
+            print ('--Inside Temp: %.1f°| color=black' % convert_temp(climate_state['inside_temp']))
+        except:
+            print ('--Inside Temp: Unavailable')
+        try:
+            print ('--Outside Temp: %.1f°| color=black' % convert_temp(climate_state['outside_temp']))
+        except:
+            print ('--Outside Temp: Unavailable')
+
+        if climate_state['is_climate_on']:
+            print ('--Stop HVAC | refresh=true terminal=false bash=%s param1=%s param2=auto_conditioning_stop' % (sys.argv[0], str(i)))
+        else:
+            print ('--Start HVAC | refresh=true terminal=false bash=%s param1=%s param2=auto_conditioning_start' % (sys.argv[0], str(i)))
+
+
+if __name__ == '__main__':
+    main()
